6fc2aa3a3b3cdbb808e470afddac5de8c2a6244c
Eric Álvarez Chinchilla <eric.alvarez.chinchilla@gmail.com>
2023-05-15 01:58:16 +0200

    feat: 05-routing solution

--- src/screens/not-found.exercise.js
+++ src/screens/not-found.exercise.js
@@ -1,11 +1,6 @@
 /** @jsx jsx */
-import {jsx} from '@emotion/core'
-
-// This page displays a regular link on the page, and we've got a styled component
-// for that.
-// 🐨 Import the Link from 'components/lib'
-// 💰 and feel free to take a peek at the implementation of that styled component
-// to be reminded how you can create styled components out of existing components
+import { jsx } from '@emotion/core'
+import { Link } from '../components/lib'
 
 function NotFoundScreen() {
   return (
@@ -18,11 +13,11 @@ function NotFoundScreen() {
       }}
     >
       <div>
-        Sorry... nothing here.
-        {/* 🐨 add a <Link> here that says "Go home" and sends the user to "/discover" */}
+        <p>Sorry... nothing here.</p>
+        <Link to="/discover">Go home</Link>
       </div>
     </div>
   )
 }
 
-export {NotFoundScreen}
+export { NotFoundScreen }
