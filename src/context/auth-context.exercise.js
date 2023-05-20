8cf8f198d0c10f695388a27df225995c5cb1d1f0
Eric Álvarez Chinchilla <eric.alvarez.chinchilla@gmail.com>
2023-05-20 18:48:26 +0200

    feat: 07-context solution (III)

--- src/context/auth-context.exercise.js
+++ src/context/auth-context.exercise.js
@@ -1,5 +1,10 @@
 import * as React from 'react';
-export const AuthContext = React.createContext();
+import * as auth from 'auth-provider'
+import { client } from 'utils/api-client'
+import { useAsync } from 'utils/hooks'
+
+const AuthContext = React.createContext();
+AuthContext.displayName = "AuthContext";
 
 export const useAuth = () => {
     const context = React.useContext(AuthContext);
@@ -7,4 +12,44 @@ export const useAuth = () => {
         throw new Error(`useAuth must be used within a AuthContext provider `)
     }
     return context;
+}
+
+async function getUser() {
+    let user = null
+
+    const token = await auth.getToken()
+    if (token) {
+        const data = await client('me', { token })
+        user = data.user
+    }
+
+    return user
+}
+
+export const AuthProvider = ({ children }) => {
+
+    const { data: user, error, isLoading, isIdle, isError, isSuccess, run, setData } = useAsync()
+
+    React.useEffect(() => {
+        run(getUser())
+    }, [run])
+
+    const login = form => auth.login(form).then(user => setData(user))
+    const register = form => auth.register(form).then(user => setData(user))
+    const logout = () => {
+        auth.logout()
+        setData(null)
+    }
+
+    return <AuthContext.Provider value={{
+        user,
+        login,
+        register,
+        logout,
+        error,
+        isLoading,
+        isIdle,
+        isError,
+        isSuccess,
+    }}>{children}</AuthContext.Provider>
 }
