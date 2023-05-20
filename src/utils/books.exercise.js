e74da840560ad5cf8c75dc04a9ab56d9d7a68bad
Eric Álvarez Chinchilla <eric.alvarez.chinchilla@gmail.com>
2023-05-20 17:41:16 +0200

    feat: 07-context solution (I)

--- src/utils/books.exercise.js
+++ src/utils/books.exercise.js
@@ -1,7 +1,7 @@
-// 🐨 we're going to use React hooks in here now so we'll need React
-import {useQuery, queryCache} from 'react-query'
-// 🐨 get AuthContext from context/auth-context
-import {client} from './api-client'
+import * as React from 'react';
+import { useQuery, queryCache } from 'react-query'
+import { AuthContext } from 'context/auth-context'
+import { client } from './api-client'
 import bookPlaceholderSvg from 'assets/book-placeholder.svg'
 
 const loadingBook = {
@@ -13,15 +13,13 @@ const loadingBook = {
   loadingBook: true,
 }
 
-const loadingBooks = Array.from({length: 10}, (v, index) => ({
+const loadingBooks = Array.from({ length: 10 }, (v, index) => ({
   id: `loading-book-${index}`,
   ...loadingBook,
 }))
 
-// 🦉 note that this is *not* treated as a hook and is instead called by other hooks
-// So we'll continue to accept the user here.
 const getBookSearchConfig = (query, user) => ({
-  queryKey: ['bookSearch', {query}],
+  queryKey: ['bookSearch', { query }],
   queryFn: () =>
     client(`books?query=${encodeURIComponent(query)}`, {
       token: user.token,
@@ -35,36 +33,34 @@ const getBookSearchConfig = (query, user) => ({
   },
 })
 
-// 💣 remove the user argument here
-function useBookSearch(query, user) {
-  // 🐨 get the user from React.useContext(AuthContext)
+function useBookSearch(query) {
+  const { user } = React.useContext(AuthContext);
   const result = useQuery(getBookSearchConfig(query, user))
-  return {...result, books: result.data ?? loadingBooks}
+  return { ...result, books: result.data ?? loadingBooks }
 }
 
-// 💣 remove the user argument here
-function useBook(bookId, user) {
-  // 🐨 get the user from React.useContext(AuthContext)
-  const {data} = useQuery({
-    queryKey: ['book', {bookId}],
+function useBook(bookId) {
+  const { user } = React.useContext(AuthContext);
+  const { data } = useQuery({
+    queryKey: ['book', { bookId }],
     queryFn: () =>
-      client(`books/${bookId}`, {token: user.token}).then(data => data.book),
+      client(`books/${bookId}`, { token: user.token }).then(data => data.book),
   })
   return data ?? loadingBook
 }
 
-// we don't want to accept the user here anymore. Instead we'll make a new
-// hook that gets the user and then returns this function
-// (memoized with React.useCallback)
-// 🐨 create a useRefetchBookSearchQuery hook here which:
-// 1. Gets the user from the AuthContext
-// 2. Returns a memoized callback (React.useCallback) version of this
-// refetchBookSearchQuery function. It should no longer need to accept user as
-// an argument and instead lists it as a dependency.
 async function refetchBookSearchQuery(user) {
   queryCache.removeQueries('bookSearch')
   await queryCache.prefetchQuery(getBookSearchConfig('', user))
 }
+
+
+const useRefetchBookSearchQuery = () => {
+  const { user } = React.useContext(AuthContext);
+  return React.useCallback(() => refetchBookSearchQuery(user), [user])
+}
+
+
 
 const bookQueryConfig = {
   staleTime: 1000 * 60 * 60,
@@ -72,7 +68,7 @@ const bookQueryConfig = {
 }
 
 function setQueryDataForBook(book) {
-  queryCache.setQueryData(['book', {bookId: book.id}], book, bookQueryConfig)
+  queryCache.setQueryData(['book', { bookId: book.id }], book, bookQueryConfig)
 }
 
-export {useBook, useBookSearch, refetchBookSearchQuery, setQueryDataForBook}
+export { useBook, useBookSearch, useRefetchBookSearchQuery, setQueryDataForBook }
--- src/utils/list-items.exercise.js
+++ src/utils/list-items.exercise.js
@@ -1,16 +1,16 @@
 // 🐨 we're going to use React hooks in here now so we'll need React
-import {useQuery, useMutation, queryCache} from 'react-query'
-// 🐨 get AuthContext from context/auth-context
-import {setQueryDataForBook} from './books'
-import {client} from './api-client'
+import * as React from 'react'
+import { useQuery, useMutation, queryCache } from 'react-query'
+import { setQueryDataForBook } from './books'
+import { client } from './api-client'
+import { AuthContext } from 'context/auth-context.exercise'
 
-// 💣 remove the user argument here
-function useListItems(user) {
-  // 🐨 get the user from React.useContext(AuthContext)
-  const {data} = useQuery({
+function useListItems() {
+  const { user } = React.useContext(AuthContext)
+  const { data } = useQuery({
     queryKey: 'list-items',
     queryFn: () =>
-      client(`list-items`, {token: user.token}).then(data => data.listItems),
+      client(`list-items`, { token: user.token }).then(data => data.listItems),
     onSuccess: async listItems => {
       for (const listItem of listItems) {
         setQueryDataForBook(listItem.book)
@@ -20,9 +20,8 @@ function useListItems(user) {
   return data ?? []
 }
 
-// 💣 remove the user argument here
-function useListItem(bookId, user) {
-  // 💣 you no longer need to pass the user here
+function useListItem(bookId) {
+  const { user } = React.useContext(AuthContext)
   const listItems = useListItems(user)
   return listItems.find(li => li.bookId === bookId) ?? null
 }
@@ -33,9 +32,8 @@ const defaultMutationOptions = {
   onSettled: () => queryCache.invalidateQueries('list-items'),
 }
 
-// 💣 remove the user argument here
-function useUpdateListItem(user, options) {
-  // 🐨 get the user from React.useContext(AuthContext)
+function useUpdateListItem(options) {
+  const { user } = React.useContext(AuthContext)
   return useMutation(
     updates =>
       client(`list-items/${updates.id}`, {
@@ -49,7 +47,7 @@ function useUpdateListItem(user, options) {
 
         queryCache.setQueryData('list-items', old => {
           return old.map(item => {
-            return item.id === newItem.id ? {...item, ...newItem} : item
+            return item.id === newItem.id ? { ...item, ...newItem } : item
           })
         })
 
@@ -61,11 +59,10 @@ function useUpdateListItem(user, options) {
   )
 }
 
-// 💣 remove the user argument here
-function useRemoveListItem(user, options) {
-  // 🐨 get the user from React.useContext(AuthContext)
+function useRemoveListItem(options) {
+  const { user } = React.useContext(AuthContext)
   return useMutation(
-    ({id}) => client(`list-items/${id}`, {method: 'DELETE', token: user.token}),
+    ({ id }) => client(`list-items/${id}`, { method: 'DELETE', token: user.token }),
     {
       onMutate(removedItem) {
         const previousItems = queryCache.getQueryData('list-items')
@@ -82,12 +79,11 @@ function useRemoveListItem(user, options) {
   )
 }
 
-// 💣 remove the user argument here
-function useCreateListItem(user, options) {
-  // 🐨 get the user from React.useContext(AuthContext)
+function useCreateListItem(options) {
+  const { user } = React.useContext(AuthContext)
   return useMutation(
-    ({bookId}) => client(`list-items`, {data: {bookId}, token: user.token}),
-    {...defaultMutationOptions, ...options},
+    ({ bookId }) => client(`list-items`, { data: { bookId }, token: user.token }),
+    { ...defaultMutationOptions, ...options },
   )
 }
 
