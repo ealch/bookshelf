c7c675a918a5caea2faa189461985ef56d95999d
Eric Álvarez Chinchilla <eric.alvarez.chinchilla@gmail.com>
2023-06-04 23:56:21 +0200

    feat: 11-unit-testing solution

--- src/utils/__tests__/misc.exercise.js
+++ src/utils/__tests__/misc.exercise.js
@@ -1,2 +1,7 @@
-test.todo('formatDate formats the date to look nice')
+const { formatDate } = require('utils/misc')
+
+it("should format the date correctly", () => {
+    const date = formatDate(new Date("01/01/2023"))
+    expect(date).toBe("Jan 23")
+})
 
