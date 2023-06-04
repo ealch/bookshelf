5d9ea6834f52913932128ddfd5d773cc08040bee
Eric Álvarez Chinchilla <eric.alvarez.chinchilla@gmail.com>
2023-06-05 00:14:43 +0200

    feat: 11-unit-testing solution (II)

--- src/utils/__tests__/api-client.exercise.js
+++ src/utils/__tests__/api-client.exercise.js
@@ -16,19 +16,7 @@ jest.mock("auth-provider")
 
 const apiURL = process.env.REACT_APP_API_URL
 
-// 🐨 add a beforeAll to start the server with `server.listen()`
-beforeAll(() => {
-  server.listen();
-})
-// 🐨 add an afterAll to stop the server when `server.close()`
-afterAll(() => {
-  server.close();
-})
-// 🐨 afterEach test, reset the server handlers to their original handlers
-// via `server.resetHandlers()`
-afterEach(() => {
-  server.resetHandlers();
-})
+
 
 // 🐨 flesh these out:
 
